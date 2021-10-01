c71c0e1cee33ff08954ddd62149ecac496ab4359
Adriano Pereira <adriano.eb@hotmail.com>
2021-10-01 15:30:37 -0300

    segundo envio

--- return.php
+++ return.php
@@ -3,5 +3,4 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Content-Type: application/json");
 echo json_encode($array);
-
 exit;
